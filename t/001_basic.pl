b96556e3e2c38929ffc6fddc3c132a27a5e613a8
Bharath Rupireddy <84120084+BRupireddy@users.noreply.github.com>
2022-04-16 17:59:52 +0530

    Update 001_basic.pl
    Added missing done_testing();

--- t/001_basic.pl
+++ t/001_basic.pl
@@ -3,9 +3,12 @@
 
 use strict;
 use warnings;
+
 use PostgreSQL::Test::Utils;
-use Test::More tests => 8;
+use Test::More;
 
 program_help_ok('pg_replslotdata');
 program_version_ok('pg_replslotdata');
 program_options_handling_ok('pg_replslotdata');
+
+done_testing();
